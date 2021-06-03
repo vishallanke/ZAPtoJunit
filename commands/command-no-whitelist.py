4bec06cf292faabbbb3b5135609ebb81ede0904f
Vishal Lanke <vishal.lanke@gmail.com>
2021-06-03 14:51:50 +0530

    Sampel commands. You need to change Path

---
+++ commands/command-no-whitelist.py
@@ -0,0 +1,2 @@
+import zap_to_junit
+zap_to_junit.convert(6, 'D:\\Gitlab_Projects\\ZapToJunitNPM\\zap_scan.json', "", 'D:\\Gitlab_Projects\\ZapToJunitNPM\\zap_scan_output.xml')
---
+++ commands/command-whitelist.py
@@ -0,0 +1,2 @@
+import zap_to_junit
+zap_to_junit.convert(1, 'D:\\Gitlab_Projects\\ZapToJunitNPM\\CHECKEDIN\\data-security-report.json', "D:\\Gitlab_Projects\\ZapToJunitNPM\\CHECKEDIN\\data-whitelist.json", "true", 'D:\\Gitlab_Projects\\ZapToJunitNPM\\CHECKEDIN\\ZAP-Junit-Compatible.xml')
